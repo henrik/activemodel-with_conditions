2864f1c4219102f061d47ac2c43f5d90c2c9ffe4
Henrik Nyh <henrik@nyh.se>
2024-12-22 19:25:20 +0100

    Initial commit

---
+++ lib/rails/with_conditions.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "with_conditions/version"
+
+module Rails
+  module WithConditions
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
